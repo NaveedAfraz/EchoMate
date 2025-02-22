108130789f2e55e0a2021d6b7494f854fdd0cd14
NaveedAfraz <naveedafraz123@gmail.com>
2025-02-23 02:45:35 +0530

    latest stuff

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 dotenv.config();
 app.use(express.json());
+const { requireAuth } = require("@clerk/express");
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -12,6 +13,7 @@ app.use(
   })
 );
 
+app.use("/api", requireAuth());
 const imgRoute = require("./helper/imageUpload");
 app.use("/api/imageUpload", imgRoute);
 
